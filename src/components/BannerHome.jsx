814b897296d1a53997a5b65fe1eaa68c55815617
Ana Liotta <liotta.a.p@gmail.com>
2025-06-29 12:29:22 -0300

    arreglado provblemna de height en banner

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -6,8 +6,8 @@ import { ReservaButton } from './Buttons'
 function BannerHome(){
 
     return(
-        <section className='relative w-full h-[250px] overflow-hidden'>
-            <div className='h-[400px] absolute inset-0 flex items-center justify-center flex-col gap-10 text-white'>
+        <section className='relative w-full h-[400px] overflow-hidden'>
+            <div className=' absolute inset-0 flex items-center justify-center flex-col gap-10 text-white'>
                 <p className=' text-4xl font-bold'>Platzi Travel</p>
 
                 {/* Boton de Reserva */}
