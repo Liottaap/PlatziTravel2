bbce06c4b0eaf8915e5bbaa1d443104c29739fd8
Ana Liotta <liotta.a.p@gmail.com>
2025-06-30 20:58:04 -0300

    agregado complemento tailwind-text-shadow a titulo de banner

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -1,14 +1,12 @@
 
 import banner from '../assets/img/sanFranciscoDesktop.jpg'
-import { Buscador } from './Buscador'
 import { ReservaButton } from './Buttons'
-
 function BannerHome(){
 
     return(
         <section className='relative w-full h-[400px] overflow-hidden'>
             <div className=' absolute inset-0 flex items-center justify-center flex-col gap-10 text-white'>
-                <p className=' text-5xl font-bold drop-shadow-md'>Platzi Travel</p>
+                <p className=' text-5xl font-bold text-shadow'>Platzi Travel</p>
 
                 {/* Boton de Reserva */}
                 <ReservaButton/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,13 @@ module.exports = {
         },
       },
     },
-    plugins: [],
+      plugins: [
+        require("@designbycode/tailwindcss-text-shadow")
+        ({
+          shadowColor: "rgba(0, 0, 0, 0.8)",
+          shadowBlur: "3px",
+          shadowOffsetX: "2px",
+          shadowOffsetY: "2px",
+      })
+    ],
   };
